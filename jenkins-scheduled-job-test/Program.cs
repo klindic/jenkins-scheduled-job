91a7ac5098c0d2ca5e9938c84caa60132756b7b0
klindic <klindic94kristijan@gmail.com>
2023-05-25 22:24:31 +0200

    using AWS Secrets Manager

--- jenkins-scheduled-job-test/Program.cs
+++ jenkins-scheduled-job-test/Program.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using jenkins_scheduled_job_test.Utils;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 using System;
@@ -33,12 +38,11 @@ class Program
     // Method to send email using SendGrid
     private static async Task SendNotificationAsync()
     {
-        string apiKey = Program.CONFIG["SendGrid:ApiKey"];
-        string fromEmailAddress = Program.CONFIG["SendGrid:FromEmailAddress"];
+        EmailConfig emailConfig = await GetSecrets();
 
-        var client = new SendGridClient(apiKey);
+        var client = new SendGridClient(emailConfig.ApiKey);
 
-        var from = new EmailAddress(fromEmailAddress, "Sender");
+        var from = new EmailAddress(emailConfig.FromEmailAddress, "Sender");
         var subject = "Cancellation Notice";
         var to = new EmailAddress("jenkins-scheduled-job-test@yopmail.com", "Reciever");
         var plainTextContent = "Hello, this is to notify you that your order has been cancelled.";
@@ -55,6 +59,42 @@ class Program
         {
             Console.WriteLine($"Failed to send email. Status code: {response.StatusCode}. Response body: {await response.Body.ReadAsStringAsync()}");
         }
+    }
 
+    private static async Task<EmailConfig> GetSecrets()
+    {
+        string secretName = "jenkins-sheduled-job-test";
+        string region = "us-east-1";
+
+        var accessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID");
+        var secretKey = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY");
+
+        var credentials = new Amazon.Runtime.BasicAWSCredentials(accessKey, secretKey);
+
+        IAmazonSecretsManager client = new AmazonSecretsManagerClient(credentials, RegionEndpoint.GetBySystemName(region));
+
+
+        GetSecretValueRequest request = new GetSecretValueRequest
+        {
+            SecretId = secretName,
+            VersionStage = "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified.
+        };
+
+        GetSecretValueResponse response;
+
+        try
+        {
+            response = await client.GetSecretValueAsync(request);
+        }
+        catch (Exception e)
+        {
+            // For a list of the exceptions thrown, see
+            // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            throw e;
+        }
+
+        var emailConfig = JsonConvert.DeserializeObject<EmailConfig>(response.SecretString);
+
+        return emailConfig;
     }
 }
---
+++ jenkins-scheduled-job-test/Utils/EmailConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace jenkins_scheduled_job_test.Utils
+{
+    using Newtonsoft.Json;
+
+    internal class EmailConfig
+    {
+        [JsonProperty("SendGrid:ApiKey")]
+        public string ApiKey { get; set; }
+
+        [JsonProperty("SendGrid:FromEmailAddress")]
+        public string FromEmailAddress { get; set; }
+
+        [JsonProperty("SendGrid:FromName")]
+        public string FromName { get; set; }
+    }
+}
